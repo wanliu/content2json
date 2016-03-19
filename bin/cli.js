907289c6add0a26e66511f2b8d8409e59e92b75e
Hysios Hu <hysios@gmail.com>
2016-03-19 18:01:12 +0800

    add sort argument

--- bin/cli.js
+++ bin/cli.js
@@ -1,79 +1,63 @@
 #!/usr/bin/env node
 
-var fs = require('fs');
-var util = require('util');
 var parseArgs = require('minimist')
-var TOML = require('toml');
-var YAML = require('yamljs');
-var merge = require('merge-deep');
 
-var glob = require('glob');
-var parseHeader = require('../index').parseHeader;
+var Parser = require('../lib/parser');
 
-var argv = parseArgs(process.argv);
-
-if (argv.parse) {
-  // files = argv.parse;
-
-  console.log(parseFiles(argv.parse));  
-}
+/**
+ * sort 
+ * 
+ * Example:
+ *   --sort='date,desc'
+ *   --sort='date desc, name'
+ *  
+ */
 
 
-
-// if (argv.sortBy) {
-  
+// if (argv.sort) {
+//   var sorter = parseSort(argv.sort);
 // }
 
+/**
+ * filter
+ * 
+ * Example:
+ *    --filter='price > 188'
+ */
 // if (argv.filter) {
-  
+//   var filter = parseFilter(argv.filter);  
 // }
 
-function parseFiles(globFiles) {
-  var results = 
-  glob(globFiles, function (err, files) {
-    if (err) {
-      return console.error(err.stack);
-    }
+/**
+ * columns
+ * 
+ * Example:
+ *    --column=cover_url=>coverUrl  
+ */
 
-    files.forEach(function (file) {
-      try {
-        data = fs.readFileSync(file, { encoding: 'utf8' });
-        var pos = 0, header, type, _ref;
-            
-        while ((_ref = parseHeader(data, pos), pos = _ref[1]) &&  pos >=0) {
-          header = _ref[0], type = _ref[2];
-          var result;
-           
-          switch(type) {
-            case '+++': // toml
-              result = TOML.parse(header);
-              break;
-            case '---': // yaml
-              result = YAML.parse(header);
-              break;
-            case '{': // json
-              result = JSON.parse('{' + header + '}');
-              break;
-            default: // error
-              console.error(new Error('invalid format'));
-              return ;
-          }
-          
-          
-          if (results[file]) {
-            results[file] = merge(results[file], result);
-          } else {
-            results[file] = result;
-          }
-          
-          // console.log(data.slice(pos))
-          // pos += type.length;
-        }
-      } catch (e) {
-        console.error(e.stack);
-      }
-    });
-    
-  });
-  return results;  
+// if (argv.columns) {
+//   var columner = parseColumn(argv.columns);
+// }
+
+/**
+ * schema
+ * 
+ * Exmaple:
+ * 
+ *    --schema={$fullPathname: $data} // default
+ *    or 
+ *    --schema=[$data.Values...]
+ */
+
+// if (argv.schema) {
+//   var schemer = parseSchema(argv.schema);
+// }
+
+
+function main() {
+  var argv = parseArgs(process.argv);
+  var results = Parser.parse(argv);
+  console.log(results);
 }
+
+main();
