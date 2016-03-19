9183fbb1a50d4878f4f407a0183e169166c6aaf7
Hysios Hu <hysios@gmail.com>
2016-03-19 18:27:05 +0800

    add usage print help documents

--- bin/cli.js
+++ bin/cli.js
@@ -4,6 +4,25 @@ var parseArgs = require('minimist')
 
 var Parser = require('../lib/parser');
 
+
+function usage() {
+  
+  output = "Usage: content2json [options]\n\n" +
+           "Options:\n" +
+           "   -h, --help               Help this tools\n" +
+           "   -m, --markdown=filename  Input to convert json Markdown files\n" +
+           "   -r, --sort='fields,...'  Sort the per content\'s front-matter expression\n\n" + 
+           "                              Example:\n" +
+           "                                --sort='price desc#number, date'\n\n" +
+           "                              # => sort price as number with order desc and\n" +
+           "                              second order with date order by default asc\n\n" +
+           "   -f, --filter             Filter every search select valid row with contents\n" +
+           "   -s, --schema             Output format, contains: 'array, full, file'\n";
+
+  console.log(output);
+}
+
+
 /**
  * sort 
  * 
@@ -55,9 +74,17 @@ var Parser = require('../lib/parser');
 
 
 function main() {
-  var argv = parseArgs(process.argv);
-  var results = Parser.parse(argv);
-  console.log(results);
+  try {
+    var argv = parseArgs(process.argv);
+    
+    if (argv.h || argv.help) return usage();
+    
+    var results = Parser.parse(argv);
+    console.log(results);    
+  } catch(e) {
+    console.error(e.stack);
+    usage();
+  }
 }
 
 main();
